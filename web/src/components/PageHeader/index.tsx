292e452b60277a25489880700b8d5a0120202796
jsdie099 <julianopaulo.santos@hotmail.com>
2020-08-03 23:46:45 -0300

    montando a estrutura inicial do projeto e fazendo as primeiras páginas

---
+++ web/src/components/PageHeader/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './style.css';
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg'
+
+interface PageHeaderProps{
+    title:string;/* Se não quiser obrigatório basta colocar 
+    uma interrogação antes do :, mas deve ser declarado um valor padrão */
+}
+
+const PageHeader:React.FC<PageHeaderProps> = (props) => {
+    return (
+        <header className="page-header">
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="voltar"/>
+                </Link>
+                <img src={logoImg} alt="Proffy"/>
+            </div>
+            <div className="header-content">
+                <strong>{props.title}</strong>
+                {props.children}
+            </div>
+        </header>
+    )
+}
+export default PageHeader;
